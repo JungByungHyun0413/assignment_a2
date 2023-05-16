dc615d0fa9b0f03041d1dd6048e86f48a4c7c230
JungByungHyun0413 <bhbuz@naver.com>
2023-05-16 13:05:47 +0900

    .

---
+++ day0515/src/main/java/models/emp/Emp.java
@@ -0,0 +1,18 @@
+package models.emp;
+
+import lombok.*;
+
+//@Getter
+//@Setter
+//@ToString
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@RequiredArgsConstructor
+@Data
+public class Emp {
+    private long EMPNO;
+
+    private String ENAME;
+
+    private String JOB;
+}
